eaefeefc834414ffff9a2d8708f1cd2c5c0836c1
harinii10 <cb.sc.u4cse23721@cb.students.amrita.edu>
2026-02-07 01:00:04 +0530

    Delete create_table.py

--- create_table.py
+++
@@ -1,9 +0,0 @@
-from app.database import engine
-from app.models import base  # ensures all models are registered
-
-# Create all tables
-print("Creating tables in the database...")
-
-base.Base.metadata.create_all(bind=engine)
-
-print("Tables created successfully!")
